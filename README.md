c284386b363a3c1812ab81e35779813ecdf00f8a
OrkunOlcal <orkunolcal@gmail.com>
2022-08-30 22:47:15 +0200

    added provider infura
    get current block number on ethereum blockchain

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Introduction to Web3
 
 1. install dependencies with `npm install`
+2. run `cp .env.example .env`
+3. in the `.env` file fill the key with your infura api key
+4. run `node provider.js` to get the latest block number on the ethereum blockchain
---
+++ providers.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import {ethers} from "ethers";
+
+const infuraUrl = `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`;
+
+const provider = new ethers.providers.JsonRpcProvider(infuraUrl);
+
+console.log("Current block number:", await provider.getBlockNumber());
